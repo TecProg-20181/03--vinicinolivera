a5a13ac52d4fefc914141f3f2587fa7d822af737
Vinicius Rodrigues <vinifladf@gmail.com>
2018-05-18 20:43:54 -0300

    Fazendo a verificação se o arquivo que está tentando acessar existe

--- word.py
+++ word.py
@@ -1,5 +1,6 @@
 import random
 import string
+import os.path
 
 WORDLIST_FILENAME = "palavras.txt"
 
@@ -10,9 +11,12 @@ class Word:
         Depending on the size of the word list, this function may
         take a while to finish.
         """
-        print "Loading word list from file..."
-        inFile = open(WORDLIST_FILENAME, 'r', 0)
-        line = inFile.readline()
-        wordlist = string.split(line)
-        print "  ", len(wordlist), "words loaded."
-        return random.choice(wordlist)
+        if os.path.isfile(WORDLIST_FILENAME):
+            print "Loading word list from file..."
+            inFile = open(WORDLIST_FILENAME, 'r', 0)
+            line = inFile.readline()
+            wordlist = string.split(line)
+            print "  ", len(wordlist), "words loaded."
+            return random.choice(wordlist)
+        else:
+            print "File ", WORDLIST_FILENAME, "not found!"
